ce5b6d4f928192224c91a97699ed20b7bd7868c8
Augusto <augustodesouzacardoso@gmail.com>
2024-05-03 20:56:57 -0300

    Installed some babel packages, axios for faster API calls and created Airlines.js, first real react component

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    // Get all airlines from API
+    // Update airlines in our state
+    axios.get('/api/v1/airlines.json')
+      .then(resp => setAirlines(resp.data.data))
+      .catch(resp => console.log(resp))
+    }, [airlines.length]
+  ) 
+
+  const list = airlines.map(item => {
+    return (
+      <li key={item.attributes.name}>{item.attributes.name}</li>
+    )
+  })
+
   return (
-    <div>
-      This is Airlines#index
-    </div>
+    <Fragment>
+      <div className="home">
+        <div className="header">
+          <h1>OpenFlights</h1>
+          <div className="subheader">Honest, unbiased airline reviews.</div>
+        </div>
+        <div className="grid">
+          <ul>{list}</ul>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
